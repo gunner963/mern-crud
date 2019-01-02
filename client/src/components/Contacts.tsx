b058e42bfc80d6764584368da337460bb2b4c847
Anuj Oza <anujoza@Anujs-MacBook-Pro.local>
2019-01-02 01:41:50 -0600

    style: add contacts components

---
+++ client/src/components/Contacts.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Contacts extends Component {
+  render() {
+    return <div>Contacts Component</div>;
+  }
+}
+export default Contacts;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+const Header = () => {
+  return (
+    <div style={{ width: '100%', textAlign: 'center' }}>
+      Header Component !!!
+    </div>
+  )
+}
+export default Header
