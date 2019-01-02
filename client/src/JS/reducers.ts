b407a1f8fd4364072adcdec972cf2141dfc0090c
Anuj <aoza@autorobinc.com>
2019-01-02 12:05:10 -0600

    chore: redux store config

---
+++ client/src/JS/reducers.ts
@@ -0,0 +1,10 @@
+const initialState = {}
+
+const rootReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default rootReducer
--- client/src/components/Contacts.tsx
+++ client/src/components/Contacts.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class Contacts extends Component {
   render() {
-    return <div>Contacts Component</div>;
+    return <div>Contacts Component</div>
   }
 }
-export default Contacts;
+export default Contacts
